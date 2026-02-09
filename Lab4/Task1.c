92f10b886b801eb91c893fad265f7aa7de87a27a
Wassay Ahmed <123342274+Hawkrige@users.noreply.github.com>
2026-02-09 07:18:17 +0000

    kk

---
+++ Lab4/Task1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+
+   // A university allows students to sit in exams only if attendance is 75% or above.
+   int attendance = 0;
+   printf("Enter the student's attendance");
+   scanf("%d",&attendance);
+
+   if (attendance >=75) {
+    print("Eligible for Exams");
+    
+   }
+   else {
+    printf("Not eligible for exams");
+
+   }
+
+   return 0;
