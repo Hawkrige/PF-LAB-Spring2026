ff35931b89e757899c1985e6d3ac314e8adec6a6
Wassay Ahmed <123342274+Hawkrige@users.noreply.github.com>
2026-02-11 17:59:37 +0000

    Task4

---
+++ Task4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char username[20];
+    char password[20];
+
+    // Predefined credentials
+    char correctUser[] = "admin";
+    char correctPass[] = "1234";
+
+    // Input
+    printf("Enter username: ");
+    scanf("%s", username);
+
+    printf("Enter password: ");
+    scanf("%s", password);
+
+    /
+    if (strcmp(username, correctUser) == 0 && 
+        strcmp(password, correctPass) == 0) {
+        printf("Login Successful\n");
+    } 
+    else {
+        printf("Invalid Credentials\n");
+    }
+
+    return 0;
+}
