86a68e1636e5c51d641b1efe3edae7cb0caf9a9a
Wassay Ahmed <123342274+Hawkrige@users.noreply.github.com>
2026-03-13 17:45:39 +0000

    Task1

---
+++ Lab7/task1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+    int a[5]={1,2,3,4,5};
+    int temp;
+    int i;
+
+    temp = a[4];
+
+    for(i=4;i>0;i--)
+    {
+        a[i] = a[i-1];
+    }
+
+    a[0] = temp;
+
+    for(i=0;i<5;i++)
+    {
+        printf("%d ",a[i]);
+    }
+
+    return 0;
+}
