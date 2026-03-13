1c0652609d170213df0090f3c50d134925a8a21f
Wassay Ahmed <123342274+Hawkrige@users.noreply.github.com>
2026-03-13 18:20:16 +0000

    task3

--- Lab7/task3.c
+++ Lab7/task3.c
@@ -1,69 +1,60 @@
 #include<stdio.h>
 
 int main(){
+
     int pass[10];
     int fail[10];
     int i = 0;
     int k = 0;
     int marks;
     float avgp;
-    float avgcf;
+    float avgf;
     int sum1 = 0;
     int sum2 = 0;
 
-    for(k < 10;k++){
-        printf("Enter 1 for pass student and Enter 2 for fail student or Enter -1 to exit program");
-        scanf("%d",i);
+    for(i=0;i<10;i++){
+        printf("Enter 1 for pass student and Enter 2 for fail student or Enter -1 to exit program: ");
+        scanf("%d",&k);
 
-        if(i == 1 ||i == 2){
-            printf("Enter marks")
-            scanf("%d", marks);
+        if(k == 1 || k == 2){
+            printf("Enter marks: ");
+            scanf("%d",&marks);
 
-            if(i == 1){
-                pass[k]= marks;
-                
+            if(k == 1){
+                pass[i] = marks;
             }
-            else if(i == 2){
-                fail[k] = marks;
+            else if(k == 2){
+                fail[i] = marks;
             }
-
-
-        
         }
 
-        else if(i == -1){
+        else if(k == -1){
             break;
         }
-
-        
-
-        
-
-    
     }
-    for(int j = 0; j<10 ; j++){
-        printf("%d Pass students marks: ",pass[j]);
 
-
+    printf("Pass students marks: ");
+    for(int j = 0; j < 10 ; j++){
+        printf("%d ",pass[j]);
     }
-    /n;
+    printf("\n");
 
-    for(int z = 0;z < 10;z++){
-        printf("%d Fail students marks: ",fail[z]);
-
+    printf("Fail students marks: ");
+    for(int z = 0; z < 10; z++){
+        printf("%d ",fail[z]);
     }
-    /n;
+    printf("\n");
 
-    for(int x = 0;x < 10;x++){
+    for(int x = 0; x < 10; x++){
         sum1 = sum1 + pass[x];
         sum2 = sum2 + fail[x];
-
-        avgp = sum1/10;
-        avgf = sum2/10;
-
-        printf("%f average of Pass students",avgp);
-        printf("%f average of fail students",avgf);
-        
     }
 
+    avgp = sum1/10.0;
+    avgf = sum2/10.0;
+
+    printf("Average of Pass students: %.2f\n",avgp);
+    printf("Average of Fail students: %.2f\n",avgf);
+
+    return 0;
 }
