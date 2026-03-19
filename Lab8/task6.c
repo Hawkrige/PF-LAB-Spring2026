e6428ff299bcf6ef56cf6ccda849c99a2baf4163
Wassay Ahmed <123342274+Hawkrige@users.noreply.github.com>
2026-03-19 18:19:35 +0000

    task6

--- Lab8/task6.c
+++ Lab8/task6.c
@@ -4,27 +4,23 @@ int main() {
     int r1, c1, r2, c2;
     int A[3][3], B[3][3], C[3][3] = {0};
 
-    
     printf("Enter rows and columns of Matrix A (max 3x3): ");
     scanf("%d %d", &r1, &c1);
 
     printf("Enter rows and columns of Matrix B (max 3x3): ");
     scanf("%d %d", &r2, &c2);
 
-    
     if (r1 > 3 || c1 > 3 || r2 > 3 || c2 > 3) {
         printf("Error: Maximum size is 3x3.\n");
         return 0;
     }
 
-    
     if (c1 != r2) {
         printf("Error: Matrix multiplication not possible.\n");
         printf("Columns of A must equal rows of B.\n");
         return 0;
     }
 
-    
     printf("\nEnter elements of Matrix A:\n");
     for (int i = 0; i < r1; i++) {
         for (int j = 0; j < c1; j++) {
@@ -32,7 +28,6 @@ int main() {
         }
     }
 
-    
     printf("\nEnter elements of Matrix B:\n");
     for (int i = 0; i < r2; i++) {
         for (int j = 0; j < c2; j++) {
@@ -40,15 +35,14 @@ int main() {
         }
     }
 
-    
+    // FIXED PART 👇
     for (int i = 0; i < r1; i++) {
         for (int j = 0; j < c2; j++) {
             for (int k = 0; k < c1; k++) {
                 C[i][j] += A[i][k] * B[k][j];
-
+            }
         }
     }
-
 
     printf("\nMatrix A:\n");
     for (int i = 0; i < r1; i++) {
@@ -58,7 +52,6 @@ int main() {
         printf("\n");
     }
 
-    
     printf("\nMatrix B:\n");
     for (int i = 0; i < r2; i++) {
         for (int j = 0; j < c2; j++) {
@@ -67,7 +60,6 @@ int main() {
         printf("\n");
     }
 
-    
     printf("\nResult Matrix (A × B):\n");
     for (int i = 0; i < r1; i++) {
         for (int j = 0; j < c2; j++) {
