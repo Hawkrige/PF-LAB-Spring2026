99dc5e3bd0726b976a452529213b17634f437d03
Wassay Ahmed <123342274+Hawkrige@users.noreply.github.com>
2026-04-06 12:11:59 +0000

    task7

---
+++ Lab9/task7.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int main(){
+    int arr[6];
+    int *p = arr;
+
+    for(int i = 0;i<6;i++)
+    {
+        printf("Enter marks:\n ");
+        scanf("%d",(p+i));
+    }
+
+    for(int j = 0; j<6;j++)
+    {
+        printf("original marks : \n %d ",*(p+j));
+
+    }
+
+    int highest;
+    highest = *p;
+    int newmark = 0;
+    
+    for(int k = 0;k<6; k++)
+    {
+
+    if(*(p+k)> highest)
+    {
+        highest = *(p+k);
+    }
+    }
+    printf("Scaled marks: \n");
+
+    for(int x = 0 ; x<6;x++)
+    {
+        newmark = ((float)*(p+x)/ highest) * 100;
+        
+        *(p+x) = (int)(newmark);
+
+        
+
+        printf("%d", *(p+x));
+        
+    }
+
+    
+return 0;
+
+}
